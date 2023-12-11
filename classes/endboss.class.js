2ac203e493e0215439a70d69cd29a32e9ddc0082
devkoepeli <devrim_kopeli@hotmail.com>
2023-12-11 12:36:48 +0100

    new class: endboss

---
+++ classes/endboss.class.js
@@ -0,0 +1,36 @@
+class Endboss extends MovableObject {
+    IMAGES_WALKING = [
+        './img/4_enemie_boss_chicken/1_walk/G1.png',
+        './img/4_enemie_boss_chicken/1_walk/G2.png',
+        './img/4_enemie_boss_chicken/1_walk/G3.png',
+        './img/4_enemie_boss_chicken/1_walk/G4.png'
+    ];
+    IMAGES_ALERT = [
+        './img/4_enemie_boss_chicken/2_alert/G5.png',
+        './img/4_enemie_boss_chicken/2_alert/G6.png',
+        './img/4_enemie_boss_chicken/2_alert/G7.png',
+        './img/4_enemie_boss_chicken/2_alert/G8.png',
+        './img/4_enemie_boss_chicken/2_alert/G9.png',
+        './img/4_enemie_boss_chicken/2_alert/G10.png',
+        './img/4_enemie_boss_chicken/2_alert/G11.png',
+        './img/4_enemie_boss_chicken/2_alert/G12.png'
+    ];
+
+    constructor() {
+        super().loadImage(this.IMAGES_ALERT[0]);
+        this.loadImages(this.IMAGES_ALERT);
+
+        this.x = 500;
+        this.y = 50;
+        this.width = 275;
+        this.height = 400;
+
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_ALERT);
+        }, 200);
+    }
+}
